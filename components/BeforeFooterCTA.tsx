c84d7609ddad7be947e2438007809df46f747390
Mustafa-synergos <mustafa@synergostech.com>
2026-06-04 16:59:23 +0530

    Add files via upload

--- components/BeforeFooterCTA.tsx
+++ components/BeforeFooterCTA.tsx
@@ -32,7 +32,7 @@ export default function BeforeFooterCTA() {
       background="custom" 
       id="final-cta" 
       customBgColor="bg-[#171717]" 
-      className="h-[125vh] lg:h-[120vh] md:h-[100vh]"
+      className="h-[125vh] lg:h-[120vh] md:h-[80vh]"
       sectionRef={sectionRef}
       backgroundElement={<InteractiveDots variant="dark" containerRef={sectionRef} />}
     >
