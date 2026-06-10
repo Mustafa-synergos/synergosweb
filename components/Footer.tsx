005c95ff4305d057ac4ceff9b5fd8d90f4db8d52
Mustafa-synergos <mustafa@synergostech.com>
2026-06-10 16:08:18 +0530

    Add files via upload

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -45,7 +45,7 @@ export default function Footer() {
           {/* BROWSE */}
           <div>
             <h3 className="font-[clother] font-normal not-italic text-[18px] sm:text-[22px] leading-[100%] tracking-normal text-white/90 mb-4 sm:mb-6 uppercase">BROWSE</h3>
-            <div className="grid grid-cols-2 sm:grid-cols-1 md: gap-x-4 gap-y-3">
+            <div className="grid grid-cols-1 sm:grid-cols-1 md: gap-x-4 gap-y-3">
               <div className="space-y-3">
                 <div><Link href="#" className="font-[clother] font-light not-italic text-[16px] sm:text-[18px] leading-[22px] sm:leading-[26px] tracking-normal text-gray-400 hover:text-white transition-colors duration-300 whitespace-nowrap">Who we are</Link></div>
                 <div><Link href="#" className="font-[clother] font-light not-italic text-[16px] sm:text-[18px] leading-[22px] sm:leading-[26px] tracking-normal text-gray-400 hover:text-white transition-colors duration-300 whitespace-nowrap">What we offer</Link></div>
@@ -62,7 +62,7 @@ export default function Footer() {
           {/* WHAT WE OFFER */}
           <div>
             <h3 className="font-[clother] font-normal not-italic text-[18px] sm:text-[22px] leading-[100%] tracking-normal text-white/90 mb-4 sm:mb-6 uppercase">WHAT WE OFFER</h3>
-            <div className="grid grid-cols-2 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-x-4 gap-y-3">
+            <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-x-4 gap-y-3">
               <div className="space-y-3">
                 <div><Link href="#" className="font-[clother] font-light not-italic text-[16px] sm:text-[18px] leading-[22px] sm:leading-[26px] tracking-normal text-gray-400 hover:text-white transition-colors duration-300 whitespace-nowrap">Strategy & Research</Link></div>
                 <div><Link href="#" className="font-[clother] font-light not-italic text-[16px] sm:text-[18px] leading-[22px] sm:leading-[26px] tracking-normal text-gray-400 hover:text-white transition-colors duration-300 whitespace-nowrap">Storytelling & Performance</Link></div>
